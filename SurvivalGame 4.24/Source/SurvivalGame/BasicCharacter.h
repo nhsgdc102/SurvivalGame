0b8af3f496f083bd199aceaecb1c1cf34d6c6ecf
necromancer512 <43522920+necromancer512@users.noreply.github.com>
2020-03-06 16:41:21 -0500

    Devin Edits
    dafgx edited today ahhahahahah help me my head hurts aaaahh

---
+++ SurvivalGame 4.24/Source/SurvivalGame/BasicCharacter.h
@@ -0,0 +1,45 @@
+// Copyright 2020 Northview High School Game Development Club
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BasicCharacter.generated.h"
+
+UCLASS()
+class SURVIVALGAME_API ABasicCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Constructor -- Sets default values for this character's properties
+	ABasicCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable)
+	//For decreasing the character's health
+		void applyDamage(float damage);
+
+	UFUNCTION(BlueprintCallable)
+	//For increasing the character's health
+		void restoreHealth(float gain);
+
+	//For making the character die
+	virtual void dieCharacter(); /*Honestly couldn't think of a professional name*/
+
+public:
+	/*Write Code Here!*/
+	//Declare health variable here (float type)
+	float health;
+
+};
